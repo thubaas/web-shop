ca0b807391bfd89e73d3f75cd5864660f9e3f7a5
thubaas <thubelihlesibanda716@gmail.com>
2022-11-06 16:02:58 +0100

    Fixed user sign-in response

--- src/main/java/dev/thubas/webshop/user/SigninResponseDto.java
+++ src/main/java/dev/thubas/webshop/user/SigninResponseDto.java
@@ -4,15 +4,17 @@ public class SigninResponseDto {
 
 	private String message;
 	private String token;
+	private Long id;
 
 	public SigninResponseDto() {
 		super();
 	}
 
-	public SigninResponseDto(String message, String token) {
+	public SigninResponseDto(String message, String token, Long id) {
 		super();
 		this.message = message;
 		this.token = token;
+		this.id = id;
 	}
 
 	public String getMessage() {
@@ -29,6 +31,14 @@ public class SigninResponseDto {
 
 	public void setToken(String token) {
 		this.token = token;
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 }
