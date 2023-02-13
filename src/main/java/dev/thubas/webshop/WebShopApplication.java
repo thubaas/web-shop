54657e2fbc462539b6a0b1b150015baeb366316d
thubaas <thubelihlesibanda716@gmail.com>
2023-02-13 16:04:44 +0200

    added jasypt encryption

--- src/main/java/dev/thubas/webshop/WebShopApplication.java
+++ src/main/java/dev/thubas/webshop/WebShopApplication.java
@@ -3,6 +3,9 @@ package dev.thubas.webshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@EnableEncryptableProperties
 @SpringBootApplication
 public class WebShopApplication {
 	
