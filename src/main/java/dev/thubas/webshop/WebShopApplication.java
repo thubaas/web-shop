d0542422a1be53fea9c0f357dff28e4ba5dd7a67
thubaas <thubelihlesibanda716@gmail.com>
2023-02-18 09:02:20 +0200

    updated db credentials for railway.app

--- src/main/java/dev/thubas/webshop/WebShopApplication.java
+++ src/main/java/dev/thubas/webshop/WebShopApplication.java
@@ -1,13 +1,10 @@
 package dev.thubas.webshop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableEncryptableProperties
 @SpringBootApplication
 public class WebShopApplication {
